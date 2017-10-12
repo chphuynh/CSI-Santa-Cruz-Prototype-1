6354b1066d639ed287f2befc046bbcbf30e37532
Christopher Huynh <chphuynh@ucsc.edu>
2017-10-12 13:04:57 -0700

    Node Script Setup

---
+++ Prototype/Assets/Scripts/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node : MonoBehaviour 
+{
+
+	public Transform cameraPosition;
+	public List<Node> reachableNodes = new List<Node>();
+
+	[HideInInspector]
+	public Collider col;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		col = GetComponent<Collider>();
+	}
+
+}
