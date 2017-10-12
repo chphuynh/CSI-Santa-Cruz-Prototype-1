70c4b9e3847646eb1f3bdc3160de1496a875a20a
Christopher Huynh <chphuynh@ucsc.edu>
2017-10-12 14:28:55 -0700

    Added Zoom out

---
+++ Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+
+	public static GameManager ins;
+
+	[HideInInspector]
+	public Node currentNode;
+
+	// Very bad singleton
+	void Awake()
+	{
+		ins = this;
+	}
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(1) && currentNode.GetComponent<Prop>() != null)
+		{
+			currentNode.GetComponent<Prop>().loc.Arrive();
+		}
+	}
+
+}
--- Prototype/Assets/Scripts/Node.cs
+++ Prototype/Assets/Scripts/Node.cs
@@ -20,8 +20,39 @@ public abstract class Node : MonoBehaviour
 
 	void OnMouseDown()
 	{
+		Arrive();
+	}
+
+	public void Arrive()
+	{
+		// Leave existing current Node
+		if(GameManager.ins.currentNode != null) GameManager.ins.currentNode.Leave();
+
+		// Set current Node
+		GameManager.ins.currentNode = this;
+		
+		// Move Camera
 		Camera.main.transform.position = cameraPosition.position;
 		Camera.main.transform.rotation = cameraPosition.rotation;
+
+		// Turn off current collider
+		if(col != null) col.enabled = false;
+
+		// Turn on colliders of reachable Nodes
+		foreach(Node node in reachableNodes)
+		{
+			if(node.col != null) node.col.enabled = true;
+		}
+
+	}
+
+	public void Leave()
+	{
+		// Turn off colliders of reachable Nodes
+		foreach(Node node in reachableNodes)
+		{
+			if(node.col != null) node.col.enabled = false;
+		}
 	}
 
 }
